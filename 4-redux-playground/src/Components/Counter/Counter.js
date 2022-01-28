5b01c7a315f4ec17cc938d94dc31b771eea446d2
djoun52 <simon.rioux.trv@gmail.com>
2022-01-28 11:00:46 +0100

    Reducer done

--- 4-redux-playground/src/Components/Counter/Counter.js
+++ 4-redux-playground/src/Components/Counter/Counter.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import {UseSelector} from 'react-redux'
+import { useSelector , useDispatch } from 'react-redux';
 
 export default function Counter() {
     
 
     const count = useSelector(state => state.count)
 
+    const dispatch = useDispatch()
+
+    const incrFunc = () =>{
+        dispatch({ 
+            type : "INCR"
+        })
+    }
+    const decrFunc = () =>{
+        dispatch({ 
+            type : "DECR"
+        })
+    }
     return (
         <div>
             <h1>Les données : {count}</h1> 
+            <button onClick={decrFunc}>-1</button>
+            <button onClick={incrFunc}>+1</button>
         </div>  
     )
 

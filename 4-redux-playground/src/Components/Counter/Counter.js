cf66ef8049f24168c43914136e2154b2d72da5ea
djoun52 <simon.rioux.trv@gmail.com>
2022-01-28 10:50:56 +0100

    useSelector done

---
+++ 4-redux-playground/src/Components/Counter/Counter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import {UseSelector} from 'react-redux'
+
+export default function Counter() {
+    
+
+    const count = useSelector(state => state.count)
+
+    return (
+        <div>
+            <h1>Les données : {count}</h1> 
+        </div>  
+    )
+
+}

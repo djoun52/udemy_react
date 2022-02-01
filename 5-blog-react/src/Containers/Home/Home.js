fc41aa106817094759e7231b1cf9e2d2f53d351b
djoun52 <simon.rioux.trv@gmail.com>
2022-02-01 15:38:46 +0100

    section 11 partie 2

---
+++ 5-blog-react/src/Containers/Home/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Home.css'
+import Card from '../../Components/Card/Card'
+
+
+export default function home() {
+
+    return (
+        <>
+            <h1 className="home-title">tout les articles</h1>
+            <div className="container-cards">
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card1</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+            </div>
+
+        </>
+    )
+}
